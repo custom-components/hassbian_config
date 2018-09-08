5524953c6859a38cb84fd4c04075672b3427850d
ludeeus <joasoe@gmail.com>
2018-09-08 16:57:23 +0200

    Moved logic to external python package

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# Custom_component for `hassbian-config`.
+# Custom_component for `hassbian-config`
 
 A custom component which allows you to controll some hassbian-config functions from Home Assistant.
   
-To get started put the contents of`/custom_components/hassbian_config/`  
-here: `<config directory>/custom_components/hassbian_config/`  
+To get started put `/custom_components/hassbian_config.py`  
+here: `<config directory>/custom_components/hassbian_config.py`  
   
 **Example configuration.yaml:**
+
 ```yaml
 hassbian_config:
 ```
-  
-For more example see the [services.yaml](https://github.com/custom-components/hassbian-config/blob/master/custom_components/hassbian_config/services.yaml) file.  
---
+++ custom_components/hassbian_config.py
@@ -0,0 +1,62 @@
+"""
+A component which allows you to controll some hassbian-config functions.
+
+For more details about this component, please refer to the documentation at
+https://github.com/ludeeus/hassbian_config
+"""
+import logging
+
+__version__ = '2.0.0'
+
+REQUIREMENTS = ['pyhassbian==0.0.3']
+
+DOMAIN = 'hassbian_config'
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup(hass, config):
+    """Setup the component."""
+    import pyhassbian
+
+    def install_suite_service(call):
+        """Define install service."""
+        suite = call.data.get('suite')
+        dev = call.data.get('dev')
+        beta = call.data.get('beta')
+        version = call.data.get('version')
+        all_suites = pyhassbian.get_suites()
+        if suite in all_suites:
+            _LOGGER.info('The suite %s is now beeing installed.', suite)
+            pyhassbian.manage_suite('install', suite, dev, beta, version)
+            _LOGGER.info('The suite %s has been installed.', suite)
+        else:
+            _LOGGER.error('The suite %s does not exist.', suite)
+
+    def upgrade_suite_service(call):
+        """Define update service."""
+        suite = call.data.get('suite')
+        dev = call.data.get('dev')
+        beta = call.data.get('beta')
+        version = call.data.get('version')
+        if suite in pyhassbian.get_suites():
+            _LOGGER.info('The suite %s is now beeing upgraded.', suite)
+            pyhassbian.manage_suite('upgrade', suite, dev, beta, version)
+            _LOGGER.info('The suite %s has been upgraded.', suite)
+        else:
+            _LOGGER.error('The suite %s does not exist.', suite)
+
+    def upgrade_os_service(call):
+        """Define update base OS service."""
+        _LOGGER.info('Upgrade of the base OS are starting.')
+        pyhassbian.os_upgrade()
+        _LOGGER.info('Upgrade of the base OS are done.')
+
+
+    if pyhassbian.is_installed():
+        _LOGGER.info('hassbian-config found, registering services.')
+        hass.services.register(DOMAIN, 'install_suite', install_suite_service)
+        hass.services.register(DOMAIN, 'upgrade_suite', upgrade_suite_service)
+        hass.services.register(DOMAIN, 'upgrade_os', upgrade_os_service)
+    else:
+        _LOGGER.error('hassbian-config not found...')
+    return True
